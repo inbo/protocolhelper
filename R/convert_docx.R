a924080bd2671e9e4b5b4a3dbb3066efa30a829e
hansvancalster <hans.vancalster@inbo.be>
2020-03-23 11:19:16 +0100

    Hard wrap lines at 80

--- R/convert_docx.R
+++ R/convert_docx.R
@@ -1,19 +1,34 @@
 #' @title Convert a docx-file (a protocol) to an (R)markdown file
 #'
-#' @description This function is derived from the [redoc::dedoc()] function and uses pandoc to convert between docx and markdown. Several options are preset to end-up with a markdown document that is in syntax as close as possible to Rmarkdown files in RStudio. During conversion, graphics (e.g. png, jpg) will be extracted from the docx archive and placed in a folder `./media` and named `image1`, `image2`, etcetera.
+#' @description This function is derived from the [redoc::dedoc()] function and
+#' uses pandoc to convert between docx and markdown.
+#' Several options are preset to end-up with a markdown document that is in
+#' syntax as close as possible to Rmarkdown files in RStudio.
+#' During conversion, graphics (e.g. png, jpg) will be extracted from the docx
+#' archive and placed in a folder `./media` and named `image1`, `image2`,
+#' etcetera.
 #'
-#' @details Metadata in the page headers and footers of the docx are ignored and will thus be lost during conversion. In case the header or footer did contain important metadata, it will need to be recovered manually. Usually header information will go inside a yaml section of an Rmarkdown.
+#' @details Metadata in the page headers and footers of the docx are ignored
+#' and will thus be lost during conversion.
+#' In case the header or footer did contain important metadata, it will need to
+#' be recovered manually.
+#' Usually header information will go inside a yaml section of an Rmarkdown.
 #'
-#' @param from The `.docx` file to convert. Can be given as an absolute or relative path.
-#' @param to The filename to write the resulting `.Rmd` file (without path). The default is to
-#' use the same basename as the docx document.
-#' @param dir The directory to write the `.Rmd` to. Defaults to current working
-#' directory. Any images will be written to `dir/media`.
-#' @param wrap The width at which to wrap text. If `NA`, text is not wrapped.
+#' @param from The `.docx` file to convert.
+#' Can be given as an absolute or relative path.
+#' @param to The filename to write the resulting `.Rmd` file (without path).
+#' The default is to use the same basename as the `.docx` document.
+#' @param dir The directory to write the `.Rmd` to.
+#' Defaults to current working directory.
+#' Any images will be written to `dir/media`.
+#' @param wrap The width at which to wrap text.
+#' If `NA`, text is not wrapped.
 #' Defaults to 80.
 #' @param overwrite Whether or not to overwrite the `to` file if it already
-#' existed. Defaults to `FALSE`.
-#' @param verbose whether to print pandoc progress text. Defaults to `FALSE`.
+#' existed.
+#' Defaults to `FALSE`.
+#' @param verbose Whether to print pandoc progress text.
+#' Defaults to `FALSE`.
 #'
 #' @importFrom rmarkdown pandoc_convert
 #' @importFrom tools file_path_sans_ext
