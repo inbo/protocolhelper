fc10915803ffb5f3253cde5ff431dad66aa9de70
Hans Van Calster <hans.vancalster@inbo.be>
2020-03-23 11:01:49 +0100

    Update R/convert_docx.R
    Co-Authored-By: Floris Vanderhaeghe <floris.vanderhaeghe@inbo.be>

--- R/convert_docx.R
+++ R/convert_docx.R
@@ -11,7 +11,7 @@
 #' directory. Any images will be written to `dir/media`.
 #' @param wrap The width at which to wrap text. If `NA`, text is not wrapped.
 #' Defaults to 80.
-#' @param overwrite Whether or not to overwrite the to file if it already
+#' @param overwrite Whether or not to overwrite the `to` file if it already
 #' existed. Defaults to FALSE.
 #' @param verbose whether to print pandoc progress text. Defaults to FALSE.
 #'
