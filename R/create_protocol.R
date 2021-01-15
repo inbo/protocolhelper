698537cd9b711f596f36197ef47026c83882dacc
hansvancalster <hans.vancalster@inbo.be>
2021-01-15 15:17:40 +0100

    add create_spp

--- R/create_protocol.R
+++ R/create_protocol.R
@@ -72,7 +72,7 @@
 #' the target folder beneath `src`.
 #' Besides Rmarkdown files, this target folder will also contain files needed to
 #' render to a Bookdown gitbook such as a `_bookdown.yml`.
-#' The `00_NEWS.Rmd` file must be used to document the changes between revisions
+#' The `NEWS.Rmd` file must be used to document the changes between revisions
 #' of the protocol.
 #' Furthermore, a `data` and a `media` folder will be created as subdirectories
 #' of the target folder.
@@ -235,6 +235,327 @@ create_sfp <- function(
                  version_number = version_number,
                  protocol_code = protocol_code,
                  theme = theme,
+                 language = language,
+                 book_filename = book_filename,
+                 output_dir = output_dir_rel,
+                 rmd_files = rmd_files
+    )
+    writeLines(map_chr(original_file_content,
+                       whisker.render,
+                       data),
+               filename)
+    close(original_file)
+  }
+
+  # docx
+  temp_filename <- "temp.Rmd"
+  if (!is.null(from_docx)) {
+    convert_docx_to_rmd(
+      from = from_docx,
+      to = temp_filename,
+      dir = path_to_protocol,
+      wrap = 80,
+      overwrite = FALSE,
+      verbose = FALSE)
+    # convert emf to png
+    emf_images <- list.files(path = file.path(path_to_protocol, "media"),
+                             pattern = ".emf",
+                             full.names = TRUE)
+    if (length(emf_images) > 0) {
+      if (!requireNamespace("magick", quietly = TRUE)) {
+        stop("Package \"magick\" needed for docx protocols with emf images. ",
+             "Please install it with 'install.packages(\"magick\")'.",
+             call. = FALSE)
+      }
+      for (img in emf_images) {
+        img_emf <- magick::image_read(path = img)
+        magick::image_write(image = img_emf,
+                            format = "png",
+                            path = str_replace(img, ".emf", ".png"))
+        file.remove(img)
+      }
+    }
+    # move relevant sections
+    contents <- readLines(con = file.path(path_to_protocol,
+                                          temp_filename))
+    contents <- str_replace_all(contents, ".emf", ".png")
+    is_title <- str_detect(string = contents, pattern = "^(#{1}\\s{1})")
+    title_numbers <- formatC(x = cumsum(is_title),
+                             width = 2, format = "d", flag = "0")
+    filenames <- str_remove(string = tolower(contents[is_title]),
+                            pattern = "^(#{1}\\s{1})")
+    filenames <- str_remove(string = filenames,
+                            pattern = "\\s$")
+    filenames <- str_replace_all(filenames, pattern = "\\s", replacement = "_")
+    filenames <- paste0(unique(title_numbers), "_", filenames, ".Rmd")
+    # delete the empty template chapters
+    file.remove(
+      list.files(
+        path = path_to_protocol,
+        pattern = "^\\d{2}_",
+        full.names = TRUE
+      )
+    )
+    # create new chapters
+    file.create(file.path(path_to_protocol, filenames))
+    # and add chapter contents from docx
+    for (chapter in unique(cumsum(is_title))) {
+      chapter_file <- file.path(path_to_protocol, filenames[chapter])
+      chapter_contents <- contents[chapter == cumsum(is_title)]
+      writeLines(text = chapter_contents,
+                 con = chapter_file)
+    }
+    # delete the complete Rmd (output of convert_docx_rmd)
+    file.remove(file.path(path_to_protocol, temp_filename))
+  }
+
+
+  # render html
+  if (render) {
+    render_protocol(protocol_folder_name = folder_name)
+  }
+  # return a message
+  if (!is.null(from_docx)) {
+    message(ifelse(
+      render,
+      "Rendering may fail if Rmarkdown files do not correspond with
+      those listed in the rmd_files field in the _bookdown.yml file.",
+      "Please check if the names of the Rmarkdown files comply with
+      those listed in the rmd_files field in the _bookdown.yml file.")
+    )
+  }
+}
+
+
+#' @title Create a folder with a bookdown (R markdown) template to start a new
+#' project protocol and optionally render to html
+#'
+#' @description This function will create a new folder based on values that are
+#' passed on via the parameters and creates a R-markdown (bookdown) skeleton
+#' based on a template file to start working on a new project-specific protocol.
+#' Optionally, the rmarkdown chapters are rendered to an html file which will
+#' be saved in a matching subfolder of the `docs` folder.
+#'
+#'
+#' @details It is assumed that the `src` folder is a subfolder of an RStudio
+#' project with git version control.
+#'
+#' @param title A character string giving the main title of the protocol
+#' @param subtitle A character string for an optional subtitle
+#' @param short_title A character string of less than 20 characters to use in
+#' folder and filenames
+#' @param authors A character vector for authors of the form First name Last
+#' name
+#' @param date A character string of the date in ISO 8601 format (YYYY-MM-DD)
+#' @param reviewers A character vector for reviewers of the form First name
+#' Last name
+#' @param file_manager A character string for the name of the document
+#' maintainer of the form First name Last name
+#' @param version_number A version number of the form `YYYY.##`.
+#' For development versions `.dev` is added.
+#' The default is `paste0(format(Sys.Date(), "%Y"), ".00.dev")` (See `from_docx`).
+#' When the protocol is ready to be released, this should be changed by a repo-
+#' admin.
+#' @param project_name A character string that is used as the folder location
+#' (`src/project/project_name`) where project-specific protocols that belong to
+#' the same project will be stored. Preferably a short name or acronym. If the
+#' folder does not exist, it will be created.
+#' @param language Language of the protocol, either `"nl"` (Dutch),
+#' the default, or `"en"` (English).
+#' @param from_docx A character string with the path (absolute or relative) to
+#' a `.docx` file containing a pre-existing protocol.
+#' Please make sure to copy-paste all relevant meta-data from the `.docx` file
+#' to the corresponding parameters of this function.
+#' If nothing is provided (i.e. default = NULL), an empty template will be used.
+#' @param protocol_number A character string giving the protocol number.
+#' This parameter should normally not be specified (i.e. NULL), unless
+#' `from_docx` is specified.
+#' A protocol number for a project-specific protocol is just a three digit string.
+#' If NULL (the default), a protocol number will be determined automatically
+#' based on pre-existing protocol numbers.
+#' Protocol numbers that are already in use can be retrieved with
+#' `get_protocolnumbers()`.
+#' @param render Whether or not to render the protocol to html.
+#' Defaults to FALSE.
+#'
+#' @importFrom rprojroot find_root is_git_root
+#' @importFrom stringr str_replace_all str_extract str_remove str_detect
+#' str_replace
+#' @importFrom assertthat assert_that is.string is.date is.flag noNA
+#' @importFrom rmarkdown draft
+#' @importFrom bookdown render_book
+#' @importFrom purrr map_chr
+#' @importFrom whisker whisker.render
+#' @importFrom fs path_rel
+#'
+#' @return A target folder to which files will be written will be created as
+#' subdirectories beneath `src`.
+#' The subfolder structure is of the form
+#' `/project/project_name/spp-protocolnumber_short_title_language/` where
+#' `project_name`, `protocolnumber`, `short_title` and `language` are determined
+#' by the corresponding arguments of the function.
+#' A matching subfolder structure will be created beneath the `docs` folder (and
+#' output files needed for rendering to html output will be placed in it if
+#' `render = TRUE`.
+#' The template Rmarkdown files, or the Rmarkdown files that result from
+#' converting a docx protocol (see `from_docx` argument), will be written to
+#' the target folder beneath `src`.
+#' Besides Rmarkdown files, this target folder will also contain files needed to
+#' render to a Bookdown gitbook such as a `_bookdown.yml`.
+#' The `NEWS.Rmd` file must be used to document the changes between revisions
+#' of the protocol.
+#' Furthermore, a `data` and a `media` folder will be created as subdirectories
+#' of the target folder.
+#' The `media` folder will contain image files extracted from the docx protocol
+#' when the `from_docx` argument is used.
+#' The `data` folder can be used to store tabular data that are needed for the
+#' protocol.
+#'
+#' @export
+create_spp <- function(
+  title,
+  subtitle,
+  short_title,
+  authors,
+  date = Sys.Date(),
+  reviewers,
+  file_manager,
+  version_number = paste0(format(Sys.Date(), "%Y"), ".00.dev"),
+  project_name,
+  language = c("nl", "en"),
+  from_docx = NULL,
+  protocol_number = NULL,
+  render = FALSE) {
+
+  # check parameters
+  assert_that(is.string(title))
+  assert_that(is.string(subtitle))
+  assert_that(is.string(short_title), nchar(short_title) <= 20)
+  assert_that(is.date(as.Date(date)))
+  assert_that(is.character(authors))
+  assert_that(is.character(reviewers))
+  assert_that(is.string(file_manager))
+  assert_that(is.string(version_number))
+  assert_that(is.string(project_name))
+  language <- match.arg(language)
+  if (!is.null(from_docx)) {
+    assert_that(is.string(from_docx),
+                file.exists(from_docx))
+  }
+  protocol_type <- "spp"
+  if (!is.null(protocol_number)) {
+    assert_that(
+      is.string(protocol_number),
+      !(protocol_number %in% get_protocolnumbers(protocol_type = protocol_type,
+                                                 language = language))
+    )
+  }
+  assert_that(is.flag(render), noNA(render))
+
+  # create protocol name
+  if (is.null(protocol_number)) {
+    all_numbers <- get_protocolnumbers(protocol_type = protocol_type,
+                                       language = language)
+    if (length(all_numbers) == 0) {
+      protocol_number <- "001"
+    } else {
+      protocol_number <- max(
+        as.integer(all_numbers),
+        0,
+        na.rm = TRUE
+      ) + 1
+      protocol_number <- formatC(protocol_number,
+                                 width = 3, format = "d", flag = "0")
+    }
+  }
+
+  short_title <- tolower(short_title)
+  short_title <- str_replace_all(short_title, " ", "-")
+  short_titles <- get_short_titles(protocol_type = protocol_type,
+                                   language = language)
+  assert_that(!(short_title %in% short_titles),
+              msg = "The given short title already exists.
+              Give a short title that is not in use.
+              Use get_short_titles() to get an overview of short titles
+              that are in use.")
+
+  protocol_code <- paste0(protocol_type, "-", protocol_number)
+  folder_name <- paste0(protocol_code, "_", short_title, "_", language)
+  folder_name <- tolower(folder_name)
+  protocol_filename <- folder_name
+
+  # directory setup
+  path_to_protocol <- get_path_to_protocol(project = project_name,
+                                           protocol_folder_name = folder_name)
+
+  # set _bookdown.yml values
+  book_filename <- paste0(protocol_filename, ".Rmd")
+  # the output_dir should be set as a relative path to make it reproducible on
+  # other machines: it should be relative to path_to_protocol
+  # first get the absolute path
+  project_root <- find_root(is_git_root)
+  output_dir <- file.path(project_root, "docs", "project", project_name,
+                          folder_name)
+  # next make it relative to path_to_protocol
+  output_dir_rel <- path_rel(output_dir, path_to_protocol)
+
+
+  # check for existence of the folders
+  if (dir.exists(path_to_protocol)) {
+    stop(sprintf(paste0("The protocol repository already has ",
+                        "a folder %s!"), path_to_protocol))
+  }
+  if (dir.exists(output_dir)) {
+    stop(sprintf(paste0("The protocol repository already has ",
+                        "a folder %s!"), output_dir))
+  }
+  # create new directories
+  dir.create(file.path(path_to_protocol),
+             recursive = TRUE)
+  dir.create(file.path(output_dir),
+             recursive = TRUE)
+  # create subfolders data and media
+  dir.create(file.path(path_to_protocol, "data"))
+  dir.create(file.path(path_to_protocol, "media"))
+
+  # move all files from the template folder
+  parent_rmd <- file.path(path_to_protocol, "index.Rmd")
+  if (language == "nl") {
+    draft(file = parent_rmd,
+          template = "template_spp_nl",
+          package = "protocolhelper",
+          edit = FALSE)
+  } else {
+    draft(file = parent_rmd,
+          template = "template_spp_en",
+          package = "protocolhelper",
+          edit = FALSE)
+  }
+
+  # create a character vector with the names of all rmd_files
+  # in correct order for compilation
+  rmd_files <- c(
+    "index.Rmd",
+    "NEWS.Rmd",
+    list.files(path = path_to_protocol,
+               pattern = "^\\d{2}.+Rmd$"))
+
+
+  # change values in parent rmarkdown and _bookdown.yml
+  filenames <- c(parent_rmd,
+                 file.path(path_to_protocol, "_bookdown.yml"))
+  for (filename in filenames) {
+    original_file <- file(filename, "r")
+    original_file_content <- readLines(filename)
+    data <- list(title = title,
+                 subtitle = subtitle,
+                 authors = authors,
+                 date = date,
+                 reviewers = reviewers,
+                 file_manager = file_manager,
+                 version_number = version_number,
+                 protocol_code = protocol_code,
+                 project_name = project_name,
                  language = language,
                  book_filename = book_filename,
                  output_dir = output_dir_rel,
