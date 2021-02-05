11e8dea83ab48a5731a940e0203117dfeeb8cab7
hansvancalster <hans.vancalster@inbo.be>
2021-02-05 17:04:28 +0100

    update function

--- R/add_subprotocol.R
+++ R/add_subprotocol.R
@@ -9,22 +9,22 @@
 #' @param protocol_code Character string giving the protocol code
 #' @param version_number Character string with format YYYY.NN
 #' @param file_name Character string with the name of the Rmarkdown
-#' file
+#' file (a chapter starting with a level 1 heading).
 #' @param section Optional character string with the name of a section within an
-#' Rmarkdown file. If not specified (the default): the whole file is taken.
+#' Rmarkdown file. If not specified (the default): the whole file is taken. It
+#' is assumed that the section has a level 2 heading.
 #' @param demote_header Number of '#' to prefix to all titles before inserting
 #' in current protocol. Default is 0. A negative value can be used to remove
 #' a '#' from all section titles
 #' @param params A list of parameter value pairs in case of parameterized
 #' protocols.
 #'
-#' @details
-#'
 #'
 #' @importFrom assertthat assert_that is.string
-#' @importFrom dplyr %>%
 #' @importFrom fs path_rel
 #' @importFrom rprojroot find_root is_git_root
+#' @importFrom stringr str_remove
+#' @importFrom purrr map %>%
 #'
 #' @export
 #'
@@ -47,7 +47,7 @@ add_subprotocol <-
            version_number,
            file_name,
            section,
-           demote_header = 0L,
+           demote_header = c(0, 1, 2, -1),
            params) {
 
     execshell <- function(commandstring, intern = FALSE) {
@@ -73,10 +73,7 @@ add_subprotocol <-
         "protocol code not in s*f-###-nl or s*f-###-en format"
       )
     }
-    assert_that(is.numeric(demote_header))
-    if (!is.integer(demote_header)) {
-      demote_header <- as.integer(demote_header)
-    }
+    demote_header <- match.arg(demote_header)
     if (!missing(params)) {
       assert_that(is.list(params))
     }
@@ -101,13 +98,64 @@ add_subprotocol <-
     execshell(paste0("git fetch ", firstremote))
 
     # get the content of the Rmd file
+    # this will return a character vector (each element is one sentence)
     rmd_content <- execshell(gitcommand,
-           intern = TRUE) %>%
-      paste0(collapse = "\n")
+           intern = TRUE)
     # What happens if this fails?
     # for instance because protocol_code and version_number don't match
 
-    # handling the section and demote arguments
+    # handling the section arguments
+    if (!missing(section)) {
+      has_section <- grepl(section, rmd_content,
+                             ignore.case = TRUE, fixed = TRUE)
+      # avoid looking in chunks which can have lines starting with '#'
+      is_chunk <- grepl("^`{3}", rmd_content)
+      i <- 1
+      while (i < length(is_chunk)) {
+          if (isTRUE(is_chunk[i]) && isTRUE(is_chunk[i+1])) {
+            i <- i + 2
+          } else if (isTRUE(is_chunk[i]) && isFALSE(is_chunk[i+1])) {
+              is_chunk[i+1] <- TRUE
+              i <- i + 1
+          } else {
+              i <- i + 1
+            }
+      }
+      # assuming section is header level 2
+      h2 <- grepl("^##\\s[A-Z]", rmd_content) & !is_chunk
+      # grab the section
+      start_section <- which(has_section)
+      if (start_section > 1) {
+        stop(
+          "section does not uniquely identify one section"
+        )
+      }
+      h2_rows <- which(h2)
+      end_section <- min(h2_rows[h2_rows > start_section]) - 1
+      rmd_content <- rmd_content[start_section:end_section]
+      is_chunk <- is_chunk[start_section:end_section]
+    }
+    # demote headers
+    if (demote_header != 0) {
+      h1 <- grepl("^#\\s[A-Z]", rmd_content) & !is_chunk
+      h2 <- grepl("^##\\s[A-Z]", rmd_content) & !is_chunk
+      h3 <- grepl("^###\\s[A-Z]", rmd_content) & !is_chunk
+      if (demote_header == -1) {
+        if(any(h1)) {
+          stop("demote header -1 not possible when a level 1 header is present")
+        } else {
+          rmd_content[h2] <- str_remove(rmd_content[h2], "^#")
+          rmd_content[h3] <- str_remove(rmd_content[h3], "^#")
+        }
+      } else {
+        rmd_content[h1] <- paste0(
+          paste0(rep("#", demote_header), collapse = ""), rmd_content[h1])
+        rmd_content[h2] <- paste0(
+          paste0(rep("#", demote_header), collapse = ""), rmd_content[h2])
+        rmd_content[h3] <- paste0(
+          paste0(rep("#", demote_header), collapse = ""), rmd_content[h3])
+      }
+    }
 
 
     # change params$... by the values supplied in params list
@@ -123,13 +171,25 @@ add_subprotocol <-
 
     # dealing with external figures and tabular data
     # extract all paths to data or media
+    data_files <- grep("data\\/\\w+\\.(csv|tsv|xls|xlsx)", rmd_content)
+    media_files <- grep("media\\/\\w+\\.(png|jpg)", rmd_content)
+    all_files <- c(data_files, media_files)
 
     # use git show to get the contents of data and media?
+    create_command <- function(path) {
+      paste0("git show ",
+             tag, ":",
+             path)
+    }
+    git_commands <- map(all_files, create_command)
+    all_files_content <- map(git_commands, execshell, intern = FALSE)
 
     # copy data and media
+    map(all_files_content, file.create)
 
     # return rmd content
-    return(rmd_content)
+    return(rmd_content %>%
+             paste0(collapse = "\n"))
   }
 
 
