8177d686dfc1d871f0aa49cd894047711b0a300c
hansvancalster <hans.vancalster@inbo.be>
2021-03-11 18:15:48 +0100

    update test add_subprotocols

--- tests/testthat/test-add_subprotocols.R
+++ tests/testthat/test-add_subprotocols.R
@@ -26,8 +26,25 @@ test_that("add dependencies of a project-specific protocol as appendix chapters 
   gert::git_tag_create(name = specific_tag, message = "bla")
   gert::git_tag_create(name = generic_tag, message = "bla")
 
-  # create a project protocol
+  # create a protocol to be used as subprotocol
   version_number <- "2020.02"
+  create_sfp(
+    title = "subsubprotocoltest", subtitle = "subtitle",
+    short_title = "vegetation 1",
+    authors = "me", reviewers = "someone else", file_manager = "who?",
+    version_number = version_number, theme = "vegetation", lang = "en"
+  )
+
+  # add, commit and tag it
+  sfp_staged <- gert::git_add(files = ".")
+  gert::git_commit_all(message = "sfp-401-en_vegetation-1")
+  specific_tag <- paste("sfp-401-en", version_number, sep = "-")
+  generic_tag <- paste("protocols", version_number, sep = "-")
+  gert::git_tag_create(name = specific_tag, message = "bla")
+  gert::git_tag_create(name = generic_tag, message = "bla")
+
+  # create a project protocol
+  version_number <- "2020.03"
   create_spp(
     title = "project protocol", subtitle = "subtitle",
     short_title = "mne protocol",
@@ -41,7 +58,7 @@ test_that("add dependencies of a project-specific protocol as appendix chapters 
 
 
   # create a second protocol to be used as subprotocol
-  version_number <- "2020.03"
+  version_number <- "2020.04"
   create_sfp(title = "Second subprotocol",
              subtitle = "",
              short_title = "second subprotocol",
@@ -80,7 +97,37 @@ test_that("add dependencies of a project-specific protocol as appendix chapters 
     file = "src/thematic/1_water/sfp-102-en_second-subprotocol/07_workflow.Rmd",
     append = TRUE)
 
+  # add a sub-subprotocol to
+  # src/thematic/1_water/sfp-102-en_second-subprotocol
+  #debugonce(add_subprotocols)
+  index <- readLines(
+    con = "src/thematic/1_water/sfp-102-en_second-subprotocol/index.Rmd")
+  index[grepl("  dependencies_protocolcode:", index)] <-
+    "  dependencies_protocolcode: ['sfp-401-en']"
+  index[grepl("  dependencies_versionnumber:", index)] <-
+    "  dependencies_versionnumber: ['2020.02']"
+  index[grepl("  dependencies_params:", index)] <-
+    "  dependencies_params: ['list()']"
+  index[grepl("  dependencies_appendix:", index)] <-
+    "  dependencies_appendix: [TRUE]"
+  writeLines(
+    index,
+    con = "src/thematic/1_water/sfp-102-en_second-subprotocol/index.Rmd")
+
+  dependencies <- data.frame(
+    protocol_code = c("sfp-401-en"),
+    version_number = c("2020.02"),
+    params = c("list()"),
+    appendix = c(TRUE))
+
+  add_subprotocols(
+    .dependencies = dependencies,
+    fetch_remote = FALSE,
+    code_mainprotocol = 'sfp-102-en')
+
+
   # add, commit and tag it
+  sfp_staged <- gert::git_add(files = ".")
   gert::git_commit_all(message = "sfp-102-en_second-subprotocol")
   specific_tag <- paste("sfp-102-en", version_number, sep = "-")
   generic_tag <- paste("protocols", version_number, sep = "-")
@@ -91,9 +138,22 @@ test_that("add dependencies of a project-specific protocol as appendix chapters 
   # add subprotocols to
   # src/project/mne/spp-001-en_mne-protocol/
   #debugonce(add_subprotocols)
+  index <- readLines(
+    con = "src/project/mne/spp-001-en_mne-protocol/index.Rmd")
+  index[grepl("  dependencies_protocolcode:", index)] <-
+    "  dependencies_protocolcode: ['sfp-101-en', 'sfp-102-en']"
+  index[grepl("  dependencies_versionnumber:", index)] <-
+    "  dependencies_versionnumber: ['2020.01', '2020.04']"
+  index[grepl("  dependencies_params:", index)] <-
+    "  dependencies_params: ['list()', 'list(reviewers = c(\"reviewer1\", \"reviewer2\"))']"
+  index[grepl("  dependencies_appendix:", index)] <-
+    "  dependencies_appendix: [TRUE, TRUE]"
+  writeLines(
+    index,
+    con = "src/project/mne/spp-001-en_mne-protocol/index.Rmd")
   dependencies <- data.frame(
     protocol_code = c("sfp-101-en", "sfp-102-en"),
-    version_number = c("2020.01","2020.03"),
+    version_number = c("2020.01","2020.04"),
     params = c("list()", "list(reviewers = c('reviewer1', 'reviewer2'))"),
     appendix = c(TRUE, TRUE))
   add_subprotocols(
@@ -102,7 +162,6 @@ test_that("add dependencies of a project-specific protocol as appendix chapters 
     code_mainprotocol = 'spp-001-en')
 
   # render spp-001-en including the subprotocol
-
   render_protocol(protocol_code = "spp-001-en",
                   params = list(
                     dependencies_protocolcode = dependencies$protocol_code,
