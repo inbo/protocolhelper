9589fa31bde1c646b21ccadca6a31de5eed35841
ElsLommelen <els.lommelen@inbo.be>
2022-05-25 11:27:34 +0200

    adapt check_structure() to use the Protocolcheck class

--- R/check_structure.R
+++ R/check_structure.R
@@ -17,8 +17,7 @@
 #' @export
 #'
 check_structure <- function(protocol_code) {
-  path_to_protocol <- get_path_to_protocol(
-    protocol_code = protocol_code)
+  x <- load_protocolcheck(protocol_code = protocol_code)
   template_name <-
     gsub(
       pattern = "(s\\wp)-\\d*-(\\w{2})_.*",
@@ -29,7 +28,7 @@ check_structure <- function(protocol_code) {
     system.file(
       file.path("rmarkdown", "templates", template_name, "skeleton"),
       package = "protocolhelper")
-  files_protocol <- list.files(path = path_to_protocol)
+  files_protocol <- list.files(path = x$path)
   files_template <- list.files(path = path_to_template)
 
   # check if file(name)s from template are conserved
@@ -40,22 +39,25 @@ check_structure <- function(protocol_code) {
     difffiles[!grep("^\\d{2}_appendices.Rmd", difffiles)]
   difffiles <-
     difffiles[!grep("^\\d{2}_subprocotols.Rmd", difffiles)]
-  assert_that(
-    length(difffiles) == 0,
-    msg = paste(protocol_code, "lacks file(s)", difffiles)
-  )
+  if (length(difffiles) > 0) {
+    x$add_error(msg = paste(protocol_code, "lacks file(s)", difffiles))
+  }
 
   for (file in files_protocol[grepl(".Rmd$", files_protocol)]) {
 
     # check if chunks in Rmd files are correct
-    rmd <- readLines(file.path(path_to_protocol, file))
+    rmd <- readLines(file.path(x$path, file))
     start_chunk <- grep("^```\\{r.*}", rmd)
     end_chunk <- grep("^```[:space:]?$", rmd)
-    assert_that(
-      length(start_chunk) == length(end_chunk),
-      all(start_chunk < end_chunk),
-      msg = paste(protocol_code, ", file", file, "has a problem with R chunks")
-    )
+    if (
+      !(length(start_chunk) == length(end_chunk) &&
+        all(start_chunk < end_chunk))
+    ) {
+      x$add_error(
+        msg =
+          paste(protocol_code, ", file", file, "has a problem with R chunks"))
+    }
+
     for (i in rev(seq_along(start_chunk))) {
       rmd <- c(
         head(rmd, start_chunk[i] - 1),
@@ -66,23 +68,25 @@ check_structure <- function(protocol_code) {
 
     # check headings general
     headings <- rmd[grepl("^[[:space:]]?#", rmd)]
-    assert_that(
-      !any(grepl("^[[:space:]]+#", headings)),
-      msg =
-        paste(
-          protocol_code, ", file", file,
-          "Headings have to start with a '#', remove the leading whitespace."
-        )
-    )
-    assert_that(
-      !any(grepl("[[:space:]]+$", headings)),
-      msg = paste(
-        protocol_code,
-        "Whitespace at the end of a heading is not allowed",
-        paste(headings[grepl("[[:space:]]+$", headings)], collapse = "\n"),
-        sep = "\n"
+    if (any(grepl("^[[:space:]]+#", headings))) {
+      x$add_error(
+        msg =
+          paste(
+            protocol_code, ", file", file,
+            "Headings have to start with a '#', remove the leading whitespace."
+          )
       )
-    )
+    }
+    if (any(grepl("[[:space:]]+$", headings))) {
+      x$add_error(
+        msg = paste(
+          protocol_code,
+          "Whitespace at the end of a heading is not allowed",
+          paste(headings[grepl("[[:space:]]+$", headings)], collapse = "\n"),
+          sep = "\n"
+        )
+      )
+    }
 
     # compare headings with template
     if (file %in% files_template) {
@@ -90,49 +94,61 @@ check_structure <- function(protocol_code) {
       headings_template <- template[grepl("^[[:space:]]?#", template)]
       headings_template <-
         headings_template[!grepl("^### Subtit", headings_template)]
-      assert_that(
-        all(headings_template %in% headings),
-        msg =
-          paste(protocol_code, "Heading(s)",
-                headings_template[!headings_template %in% headings],
-                "lack(s) in file", file)
-      )
+      if (!all(headings_template %in% headings)) {
+        x$add_error(
+          msg =
+            paste(protocol_code, "Heading(s)",
+                  headings_template[!headings_template %in% headings],
+                  "lack(s) in file", file)
+        )
+      }
+
       headings1 <- headings[grepl("^# .*", headings)]
       headings1_template <- headings_template[grepl("^# .*", headings_template)]
-      assert_that(
-        all(headings1 %in% headings1_template),
-        msg = paste(protocol_code, "Heading 1",
-                    headings1[!headings1 %in% headings1_template],
-                    "is not allowed in file", file)
-      )
-      assert_that(
-        all(headings[headings %in% headings_template] == headings_template),
-        msg = paste(protocol_code, "Headings of file", file,
-                    "are not in this order:", headings_template)
-      )
+      if (!all(headings1 %in% headings1_template)) {
+        x$add_error(
+          msg = paste(protocol_code, "Heading 1",
+                      headings1[!headings1 %in% headings1_template],
+                      "is not allowed in file", file)
+        )
+      }
+
+      if (
+        !all(headings[headings %in% headings_template] == headings_template)
+      ) {
+        x$add_error(
+          msg = paste(protocol_code, "Headings of file", file,
+                      "are not in this order:", headings_template)
+        )
+      }
+
     }
     if (file == "index.Rmd") {
       template <- readLines(file.path(path_to_template, "skeleton.Rmd"))
       template_end <-
         template[max(grep("^[[:space:]]?#", template)):length(template)]
       rmd_end <- rmd[max(grep("^[[:space:]]?#", rmd)):length(rmd)]
-      assert_that(
-        all(template_end == rmd_end),
-        msg =
-          paste(
-            protocol_code,
-            "Heading 'Metadata' or the table below have changed"
-          )
-      )
+      if (!all(template_end == rmd_end)) {
+        x$add_error(
+          msg =
+            paste(
+              protocol_code,
+              "Heading 'Metadata' or the table below have changed"
+            )
+        )
+      }
     }
   }
 
   # references
-  yml <- yaml_front_matter(file.path(path_to_protocol, "index.Rmd"))
+  yml <- yaml_front_matter(file.path(x$path, "index.Rmd"))
   if (has_name(yml, "bibliography")) {
-    for (bibfile in yml$bibliography) {
-      assert_that(bibfile %in% files_protocol,
-                  msg = paste(bibfile, "not found (needed for references)"))
-    }
+    x$add_error(
+      msg =
+        sprintf("%s not found (needed for references)",
+                yml$bibliography[!yml$bibliography %in% files_protocol])
+    )
   }
+
+  return(x)
 }
