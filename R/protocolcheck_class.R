d813267d3e228398299dcd3aa7717f5dc832cfd9
ElsLommelen <els.lommelen@inbo.be>
2022-05-30 11:51:34 +0200

    document fields and methods of Protocolcheck class

--- R/protocolcheck_class.R
+++ R/protocolcheck_class.R
@@ -9,21 +9,35 @@ protocolcheck <- R6Class(
   "Protocolcheck",
 
   public = list(
+    #' @field protocol_code Character string giving the protocol code.
     protocol_code = NULL,
+
+    #' @field path Character string giving the relative path to the protocol.
     path = NULL,
+
+    #' @field error Character vector containing all errors found in the protocol
     error = NULL,
+
+    #' @description Create a new `Protocolcheck` object.
+    #' @param protocol_code Character string giving the protocol code.
+    #' @return A new `Protocolcheck` object
     initialize = function(protocol_code) {
       self$protocol_code <- protocol_code
       self$path <- get_path_to_protocol(
         protocol_code = protocol_code)
-      invisible(self)  #werkt dat, of niet?  wat is de bedoeling?
+      invisible(self)
     },
+
+    #' @description Add a new error to the Protocolcheck object.
+    #' @param msg Error message to be added.
     add_error = function(msg) {
       self$error <- c(self$error, msg)
     }
   ),
 
-  active = list(
+    #' @description Give error report from Protocolcheck object.
+    #' @param fail Should an error be dropped if the report contains errors?
+    #' @return An error report (and if desired an error is dropped).
     check = function(fail) {
       if (length(self$error) > 0) {
         output <- c(
