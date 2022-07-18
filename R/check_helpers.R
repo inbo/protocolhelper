6cd8d0af3a389eb3e6fcf24b3f9fba7a26695b9f
hansvancalster <hans.vancalster@inbo.be>
2022-07-18 10:04:40 +0200

    add function to validate orcid

--- R/check_helpers.R
+++ R/check_helpers.R
@@ -37,3 +37,36 @@ check_protocolcode <- function(protocolcode) {
     msg = "protocol code not in s*f-###-nl or s*f-###-en format"
   )
 }
+
+
+#' @title validate an ORCID string
+#'
+#' @description Generates check digit as per ISO 7064 11,2.
+#' The last character in the ORCID iD is a checksum.
+#' In accordance with ISO/IEC 7064:2003, MOD 11-2,
+#' this checksum must be "0-9" or "X", a capital letter X which represents
+#' the value 10.
+#'
+#' @param orcid An `orcid` in the `0000-0000-0000-0000` format
+#'
+#' @return Logical. TRUE if check digit is correct.
+#' @export
+#'
+#' @examples
+#' validate_orcid("0000-0002-6378-6229")
+validate_orcid <- function(orcid) {
+  digits <- strsplit(orcid, "-")[[1]]
+  digits <- strsplit(digits, "")
+  digits <- unlist(digits)
+  check_digit <- digits[length(digits)]
+  digits <- digits[seq_along(digits[-1])]
+  digits <- as.integer(digits)
+  total <- 0
+  for (digit in digits) {
+    total <- (total + digit) * 2
+  }
+  remainder <- total %% 11
+  result <- (12 - remainder) %% 11
+  result <- ifelse(result == 10, "X", as.character(result))
+  return(check_digit == result)
+}
