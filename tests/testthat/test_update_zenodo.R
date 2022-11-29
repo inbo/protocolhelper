f4c776d45566adb4981d2f5618a21e9879f258ac
hansvancalster <hans.vancalster@inbo.be>
2022-11-29 11:17:21 +0100

    read zenodo.json file from main branch when updating in feature branch

--- tests/testthat/test_update_zenodo.R
+++ tests/testthat/test_update_zenodo.R
@@ -89,16 +89,34 @@ test_that(
       stop("please install 'gert' package for these tests to work")
     }
 
+    origin_repo <- gert::git_init(tempfile("protocol_origin"), bare = TRUE)
+    on.exit(unlink(origin_repo, recursive = TRUE), add = TRUE)
+    repo <- gert::git_clone(url = origin_repo,
+                            path = tempfile("protocol_local"), verbose = FALSE)
+    on.exit(unlink(repo, recursive = TRUE), add = TRUE)
+    old_wd <- setwd(repo)
+    on.exit(setwd(old_wd), add = TRUE)
 
-    old_wd <- getwd()
-    on.exit(setwd(old_wd))
-    test_repo <- tempfile("test_protocol")
-    dir.create(test_repo)
-    setwd(test_repo)
-    repo <- gert::git_init()
-    gert::git_config_set(name = "user.name", value = "someone")
-    gert::git_config_set(name = "user.email", value = "someone@example.org")
-    gert::git_config_set(name = "init.defaultBranch", value = "main")
+    gert::git_config_set(name = "user.name", value = "someone", repo = repo)
+    gert::git_config_set(name = "user.email", value = "someone@example.org",
+                         repo = repo)
+    file.create("NEWS.md")
+    gert::git_add("NEWS.md")
+    gert::git_commit_all(message = "add empty NEWS repo file")
+    branch_info <- gert::git_branch_list(repo = repo)
+    refspec <- branch_info$ref[
+      branch_info$name == gert::git_branch(repo = repo)]
+    gert::git_push(remote = "origin",
+                   refspec =  refspec,
+                   set_upstream = TRUE,
+                   repo = repo)
+
+    branch_info <- gert::git_branch_list(repo = repo)
+    main_branch <- ifelse(any(branch_info$name == "origin/main"),
+                          "main",
+                          ifelse(any(branch_info$name == "origin/master"),
+                                         "master", "unknown"))
+
 
     # create a protocol
     version_number <- "2021.01"
