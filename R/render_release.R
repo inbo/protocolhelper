5559288e5b9a31168970c7d352c2985a3fe2cb49
hansvancalster <hans.vancalster@inbo.be>
2025-03-27 15:45:58 +0100

    start protocolspecific doi and zenodo upload

--- R/render_release.R
+++ R/render_release.R
@@ -15,6 +15,7 @@
 #'
 #' @param output_root A character string giving the root folder without version
 #' number. Default is "publish".
+#' @param sandbox Logical. Default TRUE. Whether or not to use Zenodo sandbox.
 #'
 #' @importFrom assertthat assert_that is.string
 #' @importFrom bookdown gitbook render_book pdf_book
@@ -35,7 +36,7 @@
 #' protocolhelper:::render_release()
 #'}
 
-render_release <- function(output_root = "publish") {
+render_release <- function(output_root = "publish", sandbox = TRUE) {
   assert_that(is.string(output_root))
   assert_that(
     requireNamespace("reactable", quietly = TRUE),
@@ -186,6 +187,13 @@ render_release <- function(output_root = "publish") {
       output_dir = target_dir,
       envir = new.env()
     ) |> suppressWarnings()
+
+    # upload protocol to protocol-specific zenodo archive
+    zenrecord <- upload_zenodo(
+      yaml = yaml[[i]],
+      rendered_folder = target_dir,
+      source_folder = ".",
+      sandbox = sandbox)
   }
   protocols <- dir_ls(
     output_root, recurse = TRUE, type = "file",
---
+++ R/update_doi.R
@@ -0,0 +1,87 @@
+#' @title Update the protocol-specific Zenodo doi
+#'
+#' @description
+#' Gets and sets a doi in the yaml front matter.
+#' Not intended for interactive use.
+#' Should be used in a GitHub Action on a PR branch.
+#'
+#' @param protocol_code protocol code of the protocol that is to be published in
+#' this release
+#' @param path Default current working directory. The root folder of the
+#' `protocolsource` repo.
+#'
+#' @importFrom fs
+#' path
+#' is_dir
+#' @importFrom zen4R
+#' ZenodoManager
+#' @importFrom rlang is_empty
+#' @importFrom rmarkdown yaml_front_matter
+#'
+#' @noRd
+update_doi <- function(
+    protocol_code,
+    path = ".",
+    sandbox = TRUE,
+    token = keyring::key_get(
+      c("ZENODO_SANDBOX", "ZENODO")[c(sandbox, !sandbox)]
+    ),
+    logger = "INFO"
+) {
+  check_protocolcode(protocol_code)
+  assert_that(requireNamespace("zen4R", quietly = TRUE))
+  assert_that(requireNamespace("keyring", quietly = TRUE))
+  assert_that(is.string(path), noNA(path), is.flag(sandbox), noNA(sandbox))
+  assert_that(is_dir(path), msg = "`path` is not an existing directory")
+
+  ppath <- get_path_to_protocol(protocol_code)
+  current_branch <- git_branch(repo = path)
+
+  # read index
+  path(ppath, "index.Rmd") |>
+    readLines() -> index
+  # existing yaml
+  index_yml <- head(index, grep("---", index)[2])[-grep("---", index)[2]]
+  yml_list <- yaml_front_matter(path(ppath, "index.Rmd"))
+
+  # get reserved doi or new version doi
+  zenodo <- ZenodoManager$new(
+    token = token,
+    sandbox = sandbox,
+    logger = logger
+  )
+
+  if (rlang::is_empty(yml_list$doi)) {
+    rec <- zenodo$createEmptyRecord()
+    # de gereserveerde doi voor deze versie is dan
+    doi <- rec$pids$doi$identifier
+  } else {
+    lookup_doi <- yml_list$doi
+    myrec <- zenodo$getDepositionByDOI(lookup_doi)
+    myrec <- zenodo$depositRecordVersion(
+      myrec,
+      delete_latest_files = TRUE,
+      files = list(),
+      publish = FALSE)
+    doi <- myrec$pids$doi$identifier
+  }
+
+  # add or replace doi
+  index_yml <- c(
+    index_yml[!grepl("^doi:", index_yml)],
+    sprintf("doi: %s", doi),
+    "---"
+  )
+
+  # remove existing yaml
+  index <- tail(index, -grep("---", index)[2])
+  # add new yaml
+  index <- c(index_yml, index)
+  writeLines(index, path(ppath, "index.Rmd"))
+
+  # add, commit (optionally? can also be done in GHA docker shell)
+
+
+
+  return(invisible(doi))
+}
