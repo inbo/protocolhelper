bc4969d7e10ff0c42bf35cbb73e08a8872c40f9e
hansvancalster <hans.vancalster@inbo.be>
2025-12-04 19:32:45 +0100

    remove two packages not in package manager

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -32,7 +32,7 @@ if (tinytex::tinytex_root(error = FALSE) == "") {
     "scheme-infraonly", "selnolig", "stringenc", "symbol", "tabu",
     "tex", "tex-ini-files", "texlive-scripts", "texlive-scripts-extra",
     "texlive.infra", "threeparttable", "threeparttablex", "times",
-    "tipa", "titlesec", "tlgpg", "tlgs", "tlperl", "tocloft", "tools",
+    "tipa", "titlesec", "tlgpg", "tocloft", "tools",
     "tracklang", "trimspaces", "ulem", "unicode-data", "unicode-math",
     "uniquecounter", "url", "varwidth", "wrapfig", "xcolor", "xetex",
     "xetexconfig", "xkeyval", "xpatch", "xunicode", "zapfding"
