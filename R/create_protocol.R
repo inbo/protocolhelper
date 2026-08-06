95c32770e00dd217f5ea1634bb2c9f12a800dcd9
hansvancalster <hans.vancalster@inbo.be>
2026-08-06 09:12:20 +0200

    fix linter

--- R/create_protocol.R
+++ R/create_protocol.R
@@ -276,7 +276,7 @@ create_protocol <- function(
     paste("theme:", theme)[!is.null(theme)],
     paste("project_name:", project_name)[!is.null(project_name)],
     "community: \"inbo\"", # required by citation_meta
-    "publication_type: \"publication-technicalnote\"",#required by citation_meta
+    "publication_type: \"publication-technicalnote\"", # citation_meta
     "publisher:",
     "  - name:",
     paste0("      given: ", inbo_affiliation[[language]]),
